4ee7f16057426067e6c93599adc5d07c724350a7
Anthony Tacuri <anthony.tacuri@epn.edu.ec>
2019-04-16 19:16:38 -0500

    Ejercicio 2 Anthony Tacuri

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,11 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+function Hola($Messege){
+    echo "<center> <h1>$Messege</h1> <center>";
+}
+
+Hola("Bienvenido a mi pagina");
+Hola("Como estas Anthony");
+
 ?>
