d51e31d81f24f612b0f627bb08853b4ae3ece2af
Anthony Tacuri <anthony.tacuri@epn.edu.ec>
2019-04-16 19:08:29 -0500

    Ejercicio 1 Anthony Tacuri

--- ejercicio1/index.php
+++ ejercicio1/index.php
@@ -4,4 +4,32 @@
  * Mostrar la tabla de multiplicar del 2. Emplear el for, luego el while y por último el do/while.
  * La estructura for permite incrementar una variable de 2 en 2
  */
+
+/* ---------------for---------------*/
+echo "Bucle for <br>";
+for($i=1;$i<=12;$i++){
+    $valor=2 * $i;
+    echo "2 x $i = $valor";
+    echo "<br>";
+}
+echo "<br>";
+/* ---------------while---------------*/
+echo "Bucle while <br>";
+$i=1;
+while($i<=12){
+    $valor=2*$i;
+    echo "2 x $i = $valor";
+    echo "<br>";
+    $i++;
+}
+echo "<br>";
+/* ---------------do-while---------------*/
+echo "Bucle do-while <br>";
+$i=1;
+do{
+    $valor=2*$i;
+    echo "2 x $i = $valor";
+    echo "<br>";
+    $i++;
+}while($i<=12);
 ?>
